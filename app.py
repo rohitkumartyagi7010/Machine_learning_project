eb8ef62f4a0cc3c750cd5897cc4a45cb068c6002
Rohit <rohitkumartyagi7010@gmail.com>
2022-09-13 12:02:00 +0530

    flask

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import request,Flask ,jsonify
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+
+def index():
+
+    return "Starting machine learning project"
+
+if __name__=="__main__":
+    app.run(debug=True)
